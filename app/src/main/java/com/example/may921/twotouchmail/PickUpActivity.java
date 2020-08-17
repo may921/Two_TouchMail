ee70ff9242f589658ae7c301d61dffec3a4db625
may921 <mayzzv@yahoo.co.jp>
2020-08-17 22:55:53 +0900

    Initial commit

---
+++ app/src/main/java/com/example/may921/twotouchmail/PickUpActivity.java
@@ -0,0 +1,14 @@
+package com.example.may921.twotouchmail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PickUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_up);
+    }
+}
