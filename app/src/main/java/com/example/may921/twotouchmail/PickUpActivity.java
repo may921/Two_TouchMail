2b05463966ab8f38aebf54c653dc31bc965aa50e
may921 <mayzzv@yahoo.co.jp>
2020-08-22 00:28:31 +0900

    コミット

--- app/src/main/java/com/example/may921/twotouchmail/PickUpActivity.java
+++ app/src/main/java/com/example/may921/twotouchmail/PickUpActivity.java
@@ -2,7 +2,15 @@ package com.example.may921.twotouchmail;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class PickUpActivity extends AppCompatActivity {
 
@@ -10,5 +18,25 @@ public class PickUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick_up);
+        Button btnSend = (Button) this.findViewById(R.id.bottom);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RadioGroup rgPlace = (RadioGroup) findViewById(R.id.rg_place);
+                int checkedId = rgPlace.getCheckedRadioButtonId();
+                String strPlace = ((RadioButton) findViewById(checkedId)).getText().toString();
+                EditText edit01 = (EditText) findViewById(R.id.editText);
+                String title = edit01.getText().toString();
+                Resources res = getResources();
+                Uri uri = Uri.parse("mailto:" + res.getString(R.string.mail_to).toString());
+
+                Intent intent = new Intent(Intent.ACTION_SENDTO,uri);
+                intent.putExtra(Intent.EXTRA_SUBJECT,title);
+                intent.putExtra(Intent.EXTRA_TEXT,strPlace + "迎えに来て");
+                startActivity(intent);
+
+            }
+        });
     }
 }
