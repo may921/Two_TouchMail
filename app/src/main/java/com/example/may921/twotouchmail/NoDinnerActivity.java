14b4a4477529615d621f5c0fee07cb82fc62b733
may921 <mayzzv@yahoo.co.jp>
2020-08-24 20:56:21 +0900

    一応ほぼコードかきおえ

---
+++ app/src/main/java/com/example/may921/twotouchmail/NoDinnerActivity.java
@@ -0,0 +1,49 @@
+package com.example.may921.twotouchmail;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NoDinnerActivity extends AppCompatActivity
+    implements View.OnClickListener,View.OnLongClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_dinner);
+        Button btnSend = (Button) this.findViewById(R.id.bottom);
+        btnSend.setOnClickListener(this);
+        btnSend.setOnLongClickListener((View.OnLongClickListener) this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        EditText edit01 = (EditText)findViewById(R.id.editText);
+        String title = edit01.getText().toString();
+        Resources res = getResources();
+        Uri uri = Uri.parse("mailto:" + res.getString(R.string.mail_to).toString());
+        Intent intent = new Intent(Intent.ACTION_SENDTO,uri);
+        intent.putExtra(Intent.EXTRA_SUBJECT,title);
+        intent.putExtra(Intent.EXTRA_TEXT,"遅くなるのでご飯はいりません");
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        EditText edit01 = (EditText)findViewById(R.id.editText);
+        String title = edit01.getText().toString();
+        Resources res = getResources();
+        Uri uri = Uri.parse("mailto:" + res.getString(R.string.mail_to).toString());
+        Intent intent = new Intent(Intent.ACTION_SENDTO,uri);
+        intent.putExtra(Intent.EXTRA_SUBJECT,title);
+        intent.putExtra(Intent.EXTRA_TEXT,"遅くなるのでご飯はいりません"+"連絡遅くてごめんなさい。");
+        startActivity(intent);
+        return true;
+    }
+}
